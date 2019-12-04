1946d2e26dca5bd2695951bd68d4cffaa8b97336
Julia Cox <41338503+astrocox@users.noreply.github.com>
2019-12-04 16:53:51 -0500

    I think i did something cool!

--- main.py
+++ main.py
@@ -13,20 +13,24 @@ width, height = img.size
 new_img = Image.new("RGB", (width, height), "white")
 
 # TODO: Replace this with your own filter!
-# Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [(0, 0)] * 9
-for i in range(1, width - 1):
-    for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        members.sort()
-        new_img.putpixel((i, j), (members[4]))
+for i in range(width // 2, width):
+    for j in range(1, height // 2):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (0, 0, b))
+
+for i in range(1, width // 2):
+    for j in range(1, height // 2):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (r, r, r))
+
+for i in range(width // 2, width):
+    for j in range(height // 2, height):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (r, 0, 0))
+
+for i in range(1, width // 2):
+    for j in range(height // 2, height):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (0, g, 0))
 
 new_img.save(output_path, "JPEG")
